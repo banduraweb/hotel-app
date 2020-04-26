fcc753d1e7e9dba4dedf467c6966b33223a8dee8
bandura <banduraweb@ukr.net>
2020-04-26 22:25:27 +0300

    header login logout

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,8 +3,9 @@ import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
-import { Layout } from "antd";
+import { Affix, Layout } from "antd";
 import {
+  AppHeader,
   Home,
   Host,
   Listing,
@@ -36,6 +37,9 @@ const App = () => {
   return (
     <Router>
       <Layout id="app">
+        <Affix offsetTop={0} className="app__affix-header">
+          <AppHeader viewer={viewer} setViewer={setViewer} />
+        </Affix>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/host" component={Host} />
